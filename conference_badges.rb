16ee68d6659aac8a5aebef1642d31a2b0d51ccdb
Sha-jay Perry <shajayjanae@gmail.com>
2020-05-16 13:53:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,4 +19,12 @@ assign=[]
    return assign 
  end
  
- 
+ def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  
+  assign_rooms(attendees).each do |badge|
+    puts badge
+  end
+end 
