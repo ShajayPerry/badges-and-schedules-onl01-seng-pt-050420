91a9a9e9a245db103435d79d8283b06b7f1feeb1
Sha-jay Perry <shajayjanae@gmail.com>
2020-05-16 13:42:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,8 @@ end
 def batch_badge_creator(array)
   nuarray= ["Edsger", "Ada", "Charles", "Alan","Grace", "Linus","Matz"]
   array.each do |name|
-    array.push("Hello, my name is #{name}.")
+    nuarray.push("Hello, my name is #{name}.")
   end
-  return array 
+  return nuarray 
 end 
   
