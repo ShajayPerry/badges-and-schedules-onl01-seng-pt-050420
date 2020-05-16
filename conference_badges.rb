bf2e6acaf4b72d6f4cfc286eba1bcb6760370b05
Sha-jay Perry <shajayjanae@gmail.com>
2020-05-16 13:52:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,8 +13,8 @@ end
 
 def assign_rooms(speakers)
 assign=[]
-  
   speakers.each_with_index{|speaker,index|
   assign.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
     }
-   return assign  
+   return assign 
+ end 
