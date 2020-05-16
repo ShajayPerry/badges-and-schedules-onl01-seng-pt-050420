4f0146220ab43e8fa68d7df190d07728d760a323
Sha-jay Perry <shajayjanae@gmail.com>
2020-05-16 13:37:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,8 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end 
 def batch_badge_creator(array)
-  names= ["Edsger", "Ada", "Charles", "Alan","Grace", "Linus","Matz"]
+  array= ["Edsger", "Ada", "Charles", "Alan","Grace", "Linus","Matz"]
+  
   
 end 
   
